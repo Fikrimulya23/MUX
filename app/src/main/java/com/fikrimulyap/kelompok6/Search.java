42781eca919d2e8f71c611f7e45224df434329f1
AbiePerdanaKusuma1054 <icikiwerblaemblaem@gmail.com>
2021-01-10 23:58:12 +0700

    Search Front End

--- app/src/main/java/com/fikrimulyap/kelompok6/Search.java
+++ app/src/main/java/com/fikrimulyap/kelompok6/Search.java
@@ -1,13 +1,25 @@
 package com.fikrimulyap.kelompok6;
 
+import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
 
 //import android.support.v7.app.AppCompatActivity;
 
@@ -15,10 +27,43 @@ public class Search extends AppCompatActivity {
 
 
 
+    private static final int REQUEST_PERMISSION = 99;
+
+    ArrayList<Song> songArrayList;
+    ListView lvSongs;
+    SongsAdapter songsAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+
+
+        lvSongs = findViewById(R.id.lvSongs);
+
+        songArrayList = new ArrayList<>();
+
+        songsAdapter = new SongsAdapter(this, songArrayList);
+        lvSongs.setAdapter(songsAdapter);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION);
+            return;
+        } else {
+            getSongs();
+        }
+        lvSongs.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Song song = songArrayList.get(position);
+                Intent openMusicPlayer = new Intent(Search.this,MusicPlayerActivity.class);
+                openMusicPlayer.putExtra("song", song);
+                startActivity(openMusicPlayer);
+            }
+        });
 
         //initialize and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.Bottom_Navigation);
@@ -32,13 +77,13 @@ public class Search extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
                 switch (item.getItemId()){
+                    case R.id.search:
+                        return true;
+
                     case R.id.home:
                         startActivity(new Intent(getApplicationContext(),
                                 MainActivity.class));
                         overridePendingTransition(0,0);
-                        return true;
-
-                    case R.id.search:
                         return true;
 
                     case R.id.library:
@@ -51,5 +96,37 @@ public class Search extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getSongs();
+            }
+        }
+    }
+
+    private void getSongs() {
+
+        ContentResolver contentResolver = getContentResolver();
+        Uri songUri= MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+
+        Cursor songCursor = contentResolver.query(songUri, null, null, null, null);
+        if (songCursor != null && songCursor.moveToFirst()) {
+
+            int indexTitle = songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+            int indexArtist= songCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+            int indexData = songCursor.getColumnIndex(MediaStore.Audio.Media.DATA);
+
+            do {
+                String title = songCursor.getString(indexTitle);
+                String artist = songCursor.getString(indexArtist);
+                String path = songCursor.getString(indexData);
+                songArrayList.add(new Song(title, artist, path));
+            } while (songCursor.moveToNext());
+        }
+
+        songsAdapter.notifyDataSetChanged();
     }
 }
